9d52ffd2421ebe87cb22d89b90bdb3287d86fc7c
codefiles <paulklein87@gmail.com>
2015-04-12 22:28:57 -0400

    Update README.md

--- Optimizdit.user.js
+++ Optimizdit.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/codefiles/Optimizdit
 // @include     *www.reddit.com*
 // @version     15.04.12
-// @description Minimize threads with visited title, comment, or thmumbnail link and adds checkbox for flexibility of this feature.  Removes unused features. Open links in new tab.
+// @description Minimize threads with visited title, comment, or thmumbnail link and adds checkbox for flexibility of this feature.  Open links in new tab.  Removes unused features.
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant       GM_addStyle
 // ==/UserScript==
@@ -16,17 +16,17 @@
 		-  write code to save minimized state on visited threads for 2 days
 */
 
-/* Modifies rank class style */
-$(".rank").css('margin-left', '8px');
+/* Open links in new tab */
+$('a').attr('target', '_blank');
 
 /* Removes unused features */
 $("li.share, .expando-button").hide();
 
+/* Modifies rank class style */
+$(".rank").css('margin-left', '8px');
+
 /* Adds checkbox class style */
 GM_addStyle(".checkbox { float: left; margin-top: 18px; }");
-
-/* Open links in new tab */
-$('a').attr('target', '_blank');
 
 /* Adds div and class for checkboxes */
 $('.link').prepend($('<div>').addClass("checkbox"));
@@ -55,7 +55,7 @@ $('input[type=checkbox]').click(function() {
 		checkedbox($(this).parent().parent());
 	/* When a checkbox is unchecked */
 	} else {
-		/* Show everything under thing */
+		/* Show everything under thing class */
 		$(this).parent().siblings().show();
 		/* Show tagline class */
 		$(this).parent().siblings(".entry").children(".tagline").show();
